0134aa2afb7f15e44847ae7021d0835833fd5e3c
Sasidurka Venkatesan <venkasas@sheridancollege.ca>
2024-12-13 13:18:27 -0500

    Initial Commit

---
+++ FinalExamSasidurka/FinalExamSasidurkaApp.swift
@@ -0,0 +1,20 @@
+//
+//  FinalExamSasidurkaApp.swift
+//  FinalExamSasidurka
+//
+//  Created by Sasidurka on 2024-12-13.
+//
+
+import SwiftUI
+
+@main
+struct FinalExamSasidurkaApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
