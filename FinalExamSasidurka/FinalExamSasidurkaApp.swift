653af2186ff32b170c26505bd0e0d97d1b79269a
Sasidurka Venkatesan <venkasas@sheridancollege.ca>
2024-12-13 13:35:13 -0500

    files created

--- FinalExamSasidurka/FinalExamSasidurkaApp.swift
+++ FinalExamSasidurka/FinalExamSasidurkaApp.swift
@@ -13,8 +13,8 @@ struct FinalExamSasidurkaApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            ContactListView()
+                
         }
     }
 }
